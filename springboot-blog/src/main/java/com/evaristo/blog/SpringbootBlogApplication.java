7f06207d5a40adf12d90999d4d4222814061c30b
evaristorodrigues <evaristosrodrigues@gmail.com>
2022-02-10 00:53:40 -0300

    Finishing the project

--- springboot-blog/src/main/java/com/evaristo/blog/SpringbootBlogApplication.java
+++ springboot-blog/src/main/java/com/evaristo/blog/SpringbootBlogApplication.java
@@ -1,12 +1,17 @@
 package com.evaristo.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.evaristo.blog.entity.Role;
+import com.evaristo.blog.repository.RoleRepository;
+
 @SpringBootApplication
-public class SpringbootBlogApplication {
+public class SpringbootBlogApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper() {
@@ -15,5 +20,23 @@ public class SpringbootBlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogApplication.class, args);
 	}
-
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Role roleAdmin = new Role();		
+		roleAdmin.setName("ROLE_ADMIN");
+		
+		
+		Role roleUser = new Role();
+		roleUser.setName("ROLE_USER");
+		
+		roleRepository.save(roleAdmin);
+		roleRepository.save(roleUser);		
+	
+	}
+	
 }
