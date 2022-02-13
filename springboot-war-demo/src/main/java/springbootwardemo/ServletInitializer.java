8475e5a7f42d56a058a88c59db446f60b9c2f291
evaristorodrigues <evaristosrodrigues@gmail.com>
2022-02-13 01:03:33 -0300

    Reviewwing spring concepts

---
+++ springboot-war-demo/src/main/java/springbootwardemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package springbootwardemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootWarDemoApplication.class);
+	}
+
+}
